efcaf09d14ccc3d69659f1932d0920bc1b55b5c3
Geng Shuang <qh997@hotmail.com>
2012-04-15 12:07:04 +0800

    实现CSV文件写入。

--- getstrings.pl
+++ getstrings.pl
@@ -2,18 +2,49 @@
 
 use warnings;
 use strict;
+use Cwd;
 use File::Find;
+use XML::Parser;
+use Text::CSV;
 
-my $BASEPATH = @ARGV ? shift @ARGV : '/home/gengs/amazon/ics_mr1';
+my $BASEPATH = @ARGV ? shift @ARGV : getcwd;
+my $OUTFILE = 'test/new.csv';
 my $LANGUAGE = [
     'values',
 ];
+
+-d $BASEPATH || die "Cannot found path [$BASEPATH], $!\n";
+$BASEPATH =~ s{(?<!/)$}{/};
 
 my $prelang = join '|', @$LANGUAGE;
 $prelang =~ s/(?=[.()])/\\/g;
 
 my @file_list;
 find(\&wanted, $BASEPATH);
+
+open my $oh, ">:encoding(utf8)", $OUTFILE or die "$OUTFILE: $!";
+my $csv = Text::CSV -> new({binary => 1, eol => $/}) or die "Cannot use CSV: ".Text::CSV -> error_diag ();
+$csv -> eol("\r");
+$csv -> print($oh, ['File path', 'name', 'value']);
+
+foreach my $file (@file_list) {
+    open my $fh, "< $file" or warn "Cannot open file [$file], $!" and next;
+    my @file_content = <$fh>;
+    close $fh;
+    
+    my $file_path = $file;
+    $file_path =~ s/^$BASEPATH//;
+    
+    my $filestring = join '', @file_content;
+    while ($filestring =~ m{<str\s+name="(.*?)".*?>.*?<val>(.*?)</val>.*?</str>}xsg) {
+        my $sname = $1;
+        my $svalue = $2;
+
+        $csv -> print($oh, [$file_path, $sname, $svalue]);
+    }
+}
+
+close $oh or die "$OUTFILE: $!";
 
 sub wanted {
     if (-f $File::Find::name) {
@@ -25,7 +56,6 @@ sub wanted {
             $filename =~ s!$BASEPATH/?!!;
 
             push @file_list, $File::Find::name;
-            print $BASEPATH.'/'.$filename."\n";
         }
     }
 }
